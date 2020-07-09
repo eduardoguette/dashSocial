1324d103620d2f1df25550b1609c93a1e429a85e
Eduardo Guette <eduardoguette@github.com>
2020-07-09 16:31:53 +0200

    first commit

---
+++ precache-manifest.399181af8c5f53feeeb97f9738cca566.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0178619b6437e84537869c947d48baaa",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6bfc59ce8873c5fbf1f7",
+    "url": "/static/css/main.6a7c2df5.chunk.css"
+  },
+  {
+    "revision": "845011e2c48c3635fa1f",
+    "url": "/static/js/2.17227a14.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.17227a14.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6bfc59ce8873c5fbf1f7",
+    "url": "/static/js/main.e112226d.chunk.js"
+  },
+  {
+    "revision": "062b8280a1e0c99ee9a5",
+    "url": "/static/js/runtime-main.576ccec5.js"
+  }
+]);
